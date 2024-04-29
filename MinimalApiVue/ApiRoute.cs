1420720b8cc09771ad437aad22baf170a81550ef
Pamis Wang <pamis1116@gmail.com>
2024-04-30 00:18:38 +0800

    調整路由寫法

---
+++ MinimalApiVue/ApiRoute.cs
@@ -0,0 +1,26 @@
+﻿namespace MinimalApiVue
+{
+    public static class ApiRoute
+    {
+        /// <summary>
+        /// 應用程式路由表
+        /// </summary>
+        /// <param name="app"> WebApplication 實體物件 </param>
+        public static void UseApiRoute(this WebApplication app)
+        {
+            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = summaries[Random.Shared.Next(summaries.Length)]
+                    })
+                    .ToArray();
+                return forecast;
+            });
+        }
+    }
+}
--- MinimalApiVue/Program.cs
+++ MinimalApiVue/Program.cs
@@ -16,23 +16,8 @@ namespace MinimalApiVue
 
             app.UseAuthorization();
 
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            });
+            // 使用路由表
+            app.UseApiRoute();
 
             app.Run();
         }
