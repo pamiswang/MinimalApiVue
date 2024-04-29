a21e94ed868bda53994ff3b68dc608cb3881f6b1
Pamis Wang <pamis1116@gmail.com>
2024-04-30 00:22:46 +0800

    加入 CORS

--- MinimalApiVue/Program.cs
+++ MinimalApiVue/Program.cs
@@ -9,12 +9,27 @@ namespace MinimalApiVue
             // Add services to the container.
             builder.Services.AddAuthorization();
 
+            // 加入具有預設原則的 CORS 和中介軟體
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    policy =>
+                    {
+                        policy.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost");
+                        policy.AllowAnyHeader();
+                        policy.AllowAnyMethod();
+                        policy.AllowCredentials();
+                    });
+            });
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
 
             app.UseAuthorization();
+
+            // 啟用預設 CORS 原則
+            app.UseCors();
 
             // 使用路由表
             app.UseApiRoute();
